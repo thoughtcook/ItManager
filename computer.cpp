a59bb59ed741f2911ef153538fb18aec95c344e8
thoughtcook <thoughtcook@163.com>
2012-12-29 23:33:25 +0800

    tc

--- computer.cpp
+++ computer.cpp
@@ -7,47 +7,159 @@
 #include <iostream>
 #include "computer.h"
 
-Computer::Computer()
+#define BRAND_MAX_LEN 15
+
+//Class Memory constructor
+//default
+Memory::Memory()
 {
-    CPUFreq =  1.2; // Defoult CPU frequency: 1.2 GHz
-    MemorySize = 2.0;
-    DiskSize = 250;
+    Brand = new char[BRAND_MAX_LEN];
+    strcpy(Brand, "INITM");
+    MemorySize = 2048;
 }
 
-Computer::Computer(double CPU_freq, double Memory_size, int Disk_size)
+//Class Memory constructor
+//e.g. Memory mem0 ("Qmem", 1024);
+Memory::Memory(const char *m_brand, const int m_size)
 {
-    CPUFreq = CPU_freq;
-    MemorySize = Memory_size;
-    DiskSize = Disk_size;
+    int len = strlen(m_brand);
+    Brand = new char[len+1];
+    strcpy(Brand, m_brand);
+    MemorySize = m_size;
 }
 
-Computer::Computer(Computer &Comp)
+//Class Memory destructor
+Memory::~Memory()
 {
-     CPUFreq = Comp.CPUFreq;
-     MemorySize = Comp.MemorySize;
-     DiskSize = Comp.DiskSize;
+    delete [] Brand;
 }
 
-Computer::~Computer()
+//Class Memory construcor
+//---e.g.---Mem0 = Mem1
+Memory::Memory(const Memory& mem)
 {
+    int len = strlen(mem.Brand);
+    Brand = new char[len+1];
+    strcpy(Brand, mem.Brand);
+    MemorySize = mem.MemorySize;
 }
 
+//Class Memory method: operator+
+//---e.g.--- mem = mem0 + mem1;
+Memory Memory::operator+ (const Memory& mem) const
+{   
+    Memory memTemp;
+    memTemp.Brand = strcat(Brand, mem.Brand);
+    memTemp.MemorySize = MemorySize + mem.MemorySize;
+    return memTemp;
+}
+
+
+ostream& operator<< (ostream &os, const Memory &m)
+{
+     os << "----------Memory info-----------" << endl;
+     os << "Brand:   " << m.Brand << endl;
+     os << "Size:    " << m.MemorySize << endl;
+     return os;
+}
+
+//Class CPU constructor
+//default
+//---e.g.---CPU Intel;
+CPU::CPU()
+{
+    Brand = new char[BRAND_MAX_LEN];
+    strcpy(Brand, "INITC");
+    CPUFreq = 1.2;
+}
+
+//Class CPU constructor
+//---e.g.---CPU Intel ("Intel", 1.5);
+
+CPU::CPU(const char *c_brand, const int c_freq)
+{
+    int len = strlen(c_brand);
+    Brand = new char[len+1];
+    strcpy(Brand, c_brand);
+    CPUFreq = c_freq;
+}
+
+//Class CPU destructor
+CPU::~CPU()
+{
+    delete [] Brand;
+}
+
+ostream &operator<< (ostream &os, const CPU &c)
+{
+    os << "-----------CPU info----------" << endl;
+    os << "Brand:    " << c.Brand << endl;
+    os << "Freq:     " << c.CPUFreq << endl;
+    return os;
+}
+
+//Class Disk constuctor
+//default
+//---e.g.---Disk Disk0;
+Disk::Disk()
+{
+    Brand = new char[BRAND_MAX_LEN];
+    strcpy(Brand, "INITD");
+    DiskSize = 300;
+}
+
+//Class Disk construtor
+//---e.g.---Disk sandisk ("Sandisk", 500);
+Disk::Disk(const char *d_brand, const int d_size)
+{
+    int len = strlen(d_brand);
+    Brand = new char[len+1];
+    strcpy(Brand, d_brand);
+    DiskSize = d_size;
+}
+
+//Class Disk destructor
+Disk::~Disk()
+{
+    delete [] Brand;
+}
+
+//Class Disk method: Disk(const Disk &d)
+//---e.g.--- Disk Sandisk = westdisk;
+
+Disk::Disk(const Disk &d)
+{
+    int len = strlen(d.Brand);
+	Brand = new char[len+1];
+    strcpy(Brand, d.Brand);
+    DiskSize = d.DiskSize;
+}
+
+
+ostream &operator<< (ostream &os, const Disk &d)
+{
+    os << "------------Disk info------------" << endl;
+    os << "Brand:     " << d.Brand << endl;
+    os << "Size:      " << d.DiskSize << endl;
+    return os;
+} 
+
+//Class Computer memthod
+//Description: Update hardware
 void Computer::Update_Hw(const char *Component, const int size)
 {
-    using std::cout;
-    using std::endl;    
 
     if(strcmp(Component, "CPU") == 0)
     {
-        CPUFreq = size;
+        cCPU.update_c(size);
     }
     else if(strcmp(Component, "MEM") == 0)
     {
-        MemorySize = size;
+        cMem.update_m(size);
     }
     else if(strcmp(Component, "DISK") == 0)
     {
-        DiskSize = size;
+        cDisk.update_d(size);
     }
     else
     {
@@ -58,10 +170,8 @@ void Computer::Update_Hw(const char *Component, const int size)
 
 void Computer::Show_Hwinfo()
 {
-    using std::cout;
-    using std::endl;
-
-    cout << "CPU frequency: " << CPUFreq << endl;
-    cout << "Memory Size:   " << MemorySize << endl;
-    cout << "Disk Size::    " << DiskSize << endl;
+     cout << cCPU;
+     cout << cMem;
+     cout << cDisk;
 }
+
--- computer.h
+++ computer.h
@@ -1,28 +1,81 @@
-//Filename: computer.h
-//Filefunc: The Component of Computer Class 
-//Coder:    Huangqiang
-//Version:  v0.1
-//Date:     2012/12/25
-//Modification log:
-//
+//Filename:  computer.h
+//Filefunc:  The head file of Computer Class 
+//Coder:     Johnny Huang
+//Version:   v0.1
+//Date:      2012/12/25
+//--------------Modification Log-------------
+//--12.29.2012--Add class Memory/CPU/Disk
 
 #ifndef COMPUTER_H_
 #define COMPUTER_H_
+#include <iostream>
 
+using namespace std;
+
+//Class: Memory
+class Memory
+{
+private:
+    char *Brand;
+    int MemorySize;   //Unit: MB
+public:
+    Memory();
+    Memory(const char *m_brand, const int m_size);
+    ~Memory();
+    Memory(const Memory &mem);
+    Memory operator+ (const Memory& mem) const;
+    Memory operator+ ( const int m_size) { MemorySize += m_size; }
+    void update_m(int m_size) { MemorySize = m_size;}
+    friend ostream& operator<< (ostream &os, const Memory &m); 
+    friend Memory operator+ (const int m_size, Memory& mem)
+          { return mem+m_size; }
+};
+
+//Class: CPU
+class CPU
+{
+private:
+    char *Brand;
+    double CPUFreq; //Unit: GHz
+public:
+    CPU();
+    CPU(const char *c_brand, const int c_freq);
+    ~CPU();
+    
+   void update_c(int c_freq) {CPUFreq = c_freq;}
+   friend ostream &operator<< (ostream &os, const CPU &c);
+};
+
+//Class Disk
+class Disk
+{
+private:
+    char *Brand;
+    int DiskSize;  // Unit: GB
+public:
+    Disk();
+    Disk(const char *d_brand, const int d_size);
+    ~Disk();
+    Disk(const Disk& d);
+    Disk operator+ (const int d_size) {DiskSize += d_size;}
+    void update_d(int d_size) { DiskSize = d_size;}
+    void show() const;
+    friend Disk operator+ (const int d_size, Disk &d) { return d + d_size;}
+    friend ostream &operator << (ostream &os, const Disk &d);
+};
+
+//Class: Computer
 class Computer
 {
 private:
-   double CPUFreq;
-   double MemorySize;
-   int DiskSize;
+   CPU cCPU;
+   Memory cMem;
+   Disk cDisk;
 public:
-   Computer();
-   Computer(double CPU_freq, double Memory_size, int Disk_size);
-   Computer(Computer &Comp);
-   ~Computer();
+   Computer() : cCPU("INITC", 1.2), cMem("INITM", 1024), cDisk("INITD", 500) {}
 
-   void Update_Hw(const char *Component, const int size);
-   void Show_Hwinfo();
+   virtual void Update_Hw(const char *Component, const int size);
+   virtual void Show_Hwinfo();
 };
 
 #endif
